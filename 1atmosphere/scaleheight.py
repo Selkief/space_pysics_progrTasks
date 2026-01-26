dd53a34acd79a261e867977d72257ed9eef4a338
Selkief <s.kiefersauer@gmx.de>
2026-01-26 11:23:52 +0100

    calculates and plots variations in scaleheigt

--- 1atmosphere/scaleheight.py
+++ 1atmosphere/scaleheight.py
@@ -4,7 +4,6 @@ import numpy as np
 
 data = pd.read_csv("C:/Users/skief/Documents/UiT/semester8/space physics/progrTasks/1atmosphere/MSIS.dat",sep=r"\s+", skiprows=16)
 
-
 #calculate scaleheight from data --> H= k_B*T/(m*g)
 k_B = 1.38 * 10e-23     #[m^2*kg/(K*s^2)] boltzmanns constant
 g0 = 9.82 #[m/s] gravitational acceleration
@@ -13,8 +12,9 @@ R_E = 6378 *10e3 #[m] earth radius
 #N2 weighs 28amu or g/mol
 #O2 weighs 32amu or g/mol
 
+#define functions
 #scale height for all species
-def calc_H1(data_file, height):
+def calc_Hall(data_file, height):
      #nr density all molecules at certain height
     n = data_file.iloc[height,1]+ data_file.iloc[height,2]+ data_file.iloc[height,3]
     #average molecule weight
@@ -23,8 +23,8 @@ def calc_H1(data_file, height):
     H = (k_B * data_file.iloc[height,5]) / (m * g0)
     return H
 
-#scale height for single species
-def calc_H2(data_file, height, g, species):
+#scale height for single species, cst g or varying g
+def calc_Hsingle(data_file, height, g, species):
     if species == "O":
         m = 16 * 1.66054 * 10e-27
     elif species == "N2":
@@ -53,40 +53,88 @@ def exp_H(data_file, height, species):
         H.append(float(Hz))
     return np.nansum(H)
 
-H_0 = calc_H1(data, 0)
-H_10 = calc_H1(data, 10)
-print(f"scale height at 0km:{H_0}m, at 10km: {H_10}m")
+#calculate results
+#scaleheight for all species
+H_0 = calc_Hall(data, 0)
+H_10 = calc_Hall(data, 10)
 
+#scaleheight for single species
 #constant g:
-H_120_O = calc_H2(data, 120, g0, "O")
-H_120_N2 = calc_H2(data, 120, g0, "N2")
-H_120_O2 = calc_H2(data, 120, g0, "O2")
-H_600_O = calc_H2(data, 600, g0, "O")
-H_600_N2 = calc_H2(data, 600, g0, "N2")
-H_600_O2 = calc_H2(data, 600, g0, "O2")
+H_120_O = calc_Hsingle(data, 120, g0, "O")
+H_120_N2 = calc_Hsingle(data, 120, g0, "N2")
+H_120_O2 = calc_Hsingle(data, 120, g0, "O2")
+H_600_O = calc_Hsingle(data, 600, g0, "O")
+H_600_N2 = calc_Hsingle(data, 600, g0, "N2")
+H_600_O2 = calc_Hsingle(data, 600, g0, "O2")
 H_cstg =[H_120_O, H_120_N2, H_120_O2, H_600_O, H_600_N2, H_600_O2]
 
 #non-constant g:
-H_120_gz_O = calc_H2(data, 120, "g_z", "O")
-H_120_gz_N2 = calc_H2(data, 120, "g_z", "N2")
-H_120_gz_O2 = calc_H2(data, 120, "g_z", "O2")
-H_600_gz_O = calc_H2(data, 600, "g_z", "O")
-H_600_gz_N2 = calc_H2(data, 600, "g_z", "N2")
-H_600_gz_O2 = calc_H2(data, 600, "g_z", "O2")
+H_120_gz_O = calc_Hsingle(data, 120, "g_z", "O")
+H_120_gz_N2 = calc_Hsingle(data, 120, "g_z", "N2")
+H_120_gz_O2 = calc_Hsingle(data, 120, "g_z", "O2")
+H_600_gz_O = calc_Hsingle(data, 600, "g_z", "O")
+H_600_gz_N2 = calc_Hsingle(data, 600, "g_z", "N2")
+H_600_gz_O2 = calc_Hsingle(data, 600, "g_z", "O2")
 H_gz = [H_120_gz_O, H_120_gz_N2, H_120_gz_O2, H_600_gz_O, H_600_gz_N2, H_600_gz_O2]
+
 #difference between scaleheight with constant g and varying g (dependend on z)
 diff = []
 for i in range(6):
    diff.append(float(H_gz[i] - H_cstg[i]))
 
-#exp scaleheight:
+#exp scaleheight: #doesnt work yet
 expH_120_O = exp_H(data, 120, "O")
 exp_H_600_O = exp_H(data, 600, "O")
+
+#print solutions for part 1
+print(f"scale height at 0km:{H_0}m, at 10km: {H_10}m")
 print(f"scale height 120km cst g for O {H_120_O:.2f}m for N2 {H_120_N2:.2f}m for O2 {H_120_O2:.2f}m")
 print(f"scale height 600km cst g for O {H_600_O:.2f}m for N2 {H_600_N2:.2f}m for O2 {H_600_O2:.2f}m")
-print(f"scale height 120km cst g for O {H_120_gz_O:.2f}m for N2 {H_120_gz_N2:.2f}m for O2 {H_120_gz_O2:.2f}m")
-print(f"scale height 600km cst g for O {H_600_gz_O:.2f}m for N2 {H_600_gz_N2:.2f}m for O2 {H_600_gz_O2:.2f}m")
+print(f"scale height 120km non-cst g for O {H_120_gz_O:.2f}m for N2 {H_120_gz_N2:.2f}m for O2 {H_120_gz_O2:.2f}m")
+print(f"scale height 600km non-cst g for O {H_600_gz_O:.2f}m for N2 {H_600_gz_N2:.2f}m for O2 {H_600_gz_O2:.2f}m")
 print(f"difference between constant and non-constant g (in m):{diff}")
 
 print(f"experimental scaleheight for O at 120km {expH_120_O} and at 600km {exp_H_600_O}")
 
+#part 2: altitude variations of scaleheight
+H_all = []
+H_O = []
+H_N2 = []
+H_O2 = []
+
+for k in range(600):
+    H_all.append(float(calc_Hall(data, k))*10e-3)
+    H_O.append(float(calc_Hsingle(data, k, g0, "O"))*10e-3)
+    H_N2.append(float(calc_Hsingle(data, k, g0, "N2"))*10e-3)
+    H_O2.append((float(calc_Hsingle(data, k, g0, "O2"))*10e-3))
+#define some axis for height
+z = np.arange(600)
+
+#plot results
+fig, axs = plt.subplots(1,2)
+axs[0].plot(H_all,z)
+axs[0].grid(True)
+axs[0].set_title("scaleheight for all species")
+axs[0].set_xlabel("scaleheight in km")
+axs[0].set_ylabel("height in km")
+axs[1].plot(H_O, z)
+axs[1].grid(True)
+axs[1].set_title("scaleheight for O")
+axs[1].set_xlabel("scaleheight in km")
+axs[1].set_ylabel("height in km")
+plt.tight_layout()
+plt.show()
+
+fig, axs = plt.subplots(1,2)
+axs[0].plot(H_N2,z)
+axs[0].grid(True)
+axs[0].set_title("scaleheight for N2")
+axs[0].set_xlabel("scaleheight in km")
+axs[0].set_ylabel("height in km")
+axs[1].plot(H_O2, z)
+axs[1].grid(True)
+axs[1].set_title("scaleheight for O2")
+axs[1].set_xlabel("scaleheight in km")
+axs[1].set_ylabel("height in km")
+plt.tight_layout()
+plt.show()
