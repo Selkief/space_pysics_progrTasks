15ec2ee3c23bf9b934d473a6a0e59cdb68b755f3
Selkief <s.kiefersauer@gmx.de>
2026-01-26 10:54:52 +0100

    tried exp scaleheight as a sum

--- 1atmosphere/scaleheight.py
+++ 1atmosphere/scaleheight.py
@@ -13,6 +13,7 @@ R_E = 6378 *10e3 #[m] earth radius
 #N2 weighs 28amu or g/mol
 #O2 weighs 32amu or g/mol
 
+#scale height for all species
 def calc_H1(data_file, height):
      #nr density all molecules at certain height
     n = data_file.iloc[height,1]+ data_file.iloc[height,2]+ data_file.iloc[height,3]
@@ -22,12 +23,7 @@ def calc_H1(data_file, height):
     H = (k_B * data_file.iloc[height,5]) / (m * g0)
     return H
 
-H_0 = calc_H1(data, 0)
-H_10 = calc_H1(data, 10)
-print(f"scale height at 0km:{H_0}m, at 10km: {H_10}m")
-
 #scale height for single species
-
 def calc_H2(data_file, height, g, species):
     if species == "O":
         m = 16 * 1.66054 * 10e-27
@@ -42,6 +38,24 @@ def calc_H2(data_file, height, g, species):
         g = g0
     H = (k_B * data_file.iloc[height, 5]) / (m * g)
     return H
+
+#experimental scale height H_x = -n_x/(dn_x/dz)
+def exp_H(data_file, height, species):
+    if species == "O":
+        column = 1
+    elif species == "N2":
+        column = 2
+    elif species == "O2":
+        column = 3
+    H = []
+    for i in range(1,height):
+        Hz = (-data_file.iloc[i,column])/(data_file.iloc[i, column]-data_file.iloc[i-1, column]/10e3)
+        H.append(float(Hz))
+    return np.nansum(H)
+
+H_0 = calc_H1(data, 0)
+H_10 = calc_H1(data, 10)
+print(f"scale height at 0km:{H_0}m, at 10km: {H_10}m")
 
 #constant g:
 H_120_O = calc_H2(data, 120, g0, "O")
@@ -60,16 +74,19 @@ H_600_gz_O = calc_H2(data, 600, "g_z", "O")
 H_600_gz_N2 = calc_H2(data, 600, "g_z", "N2")
 H_600_gz_O2 = calc_H2(data, 600, "g_z", "O2")
 H_gz = [H_120_gz_O, H_120_gz_N2, H_120_gz_O2, H_600_gz_O, H_600_gz_N2, H_600_gz_O2]
-
+#difference between scaleheight with constant g and varying g (dependend on z)
 diff = []
 for i in range(6):
    diff.append(float(H_gz[i] - H_cstg[i]))
 
-
+#exp scaleheight:
+expH_120_O = exp_H(data, 120, "O")
+exp_H_600_O = exp_H(data, 600, "O")
 print(f"scale height 120km cst g for O {H_120_O:.2f}m for N2 {H_120_N2:.2f}m for O2 {H_120_O2:.2f}m")
 print(f"scale height 600km cst g for O {H_600_O:.2f}m for N2 {H_600_N2:.2f}m for O2 {H_600_O2:.2f}m")
 print(f"scale height 120km cst g for O {H_120_gz_O:.2f}m for N2 {H_120_gz_N2:.2f}m for O2 {H_120_gz_O2:.2f}m")
 print(f"scale height 600km cst g for O {H_600_gz_O:.2f}m for N2 {H_600_gz_N2:.2f}m for O2 {H_600_gz_O2:.2f}m")
 print(f"difference between constant and non-constant g (in m):{diff}")
 
+print(f"experimental scaleheight for O at 120km {expH_120_O} and at 600km {exp_H_600_O}")
 
